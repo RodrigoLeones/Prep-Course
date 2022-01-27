38e18f9eb1cd17da7298a66a3357e52965e3f9b1
RodrigoLeones <rodrigo.leones@hotmail.com.ar>
2022-01-27 01:33:30 -0300

    4 commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -26,6 +26,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (var i = 0; i < array.length; i++) {
+    array [i] = array [i] + 1
+  }
+  return array
 }
 
 
@@ -50,10 +54,10 @@ function agregarItemAlComienzoDelArray(array, elemento) {
 
 function dePalabrasAFrase(palabras) {
   // "palabras" es un array de strings/cadenas
-  // Devuelve un string donde todas las palabras estén concatenadas
-  // con espacios entre cada palabra
+  // Devuelve un string donde todas las palabras estén concatenadas con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join(" ")
 }
 
 
@@ -74,6 +78,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0
+  for (var i = 0; i < numeros.length; i++) {
+  suma = suma + numeros[i]
+}
+return suma
 }
 
 
@@ -81,6 +90,7 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  return agregarNumeros(resultadosTest)/resultadosTest.length
 }
 
 
@@ -88,6 +98,12 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var max = numeros[0]
+  for ( let i = 1; i < numeros.length; i++) {
+    if ( numeros[i] > max ) {
+      max = numeros[i]
+    }
+  return max
 }
 
 
